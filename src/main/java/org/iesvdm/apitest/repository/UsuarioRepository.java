b3d448e9cc83807300757250c79a82f84bca6d95
darioguiles <dario.guilescuesta@hotmail.com>
2024-03-04 14:38:21 +0100

    aaa

--- src/main/java/org/iesvdm/apitest/repository/UsuarioRepository.java
+++ src/main/java/org/iesvdm/apitest/repository/UsuarioRepository.java
@@ -2,14 +2,19 @@ package org.iesvdm.apitest.repository;
 
 import org.iesvdm.apitest.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository //Reforzamos el repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    //Si no usar native query con @Query
     //Por algun motivo no va?
 /*
-    public List<Usuario> findByNom_usuario (String titulo);
+    public List<Usuario> findByNom_usuarioContainingIgnoreCase (String titulo);
     public List<Usuario> findByNom_usuarioContainingIgnoreCaseOrderByNom_usuarioAsc (String titulo);
     public List<Usuario> findByNom_usuarioContainingIgnoreCaseOrderByNom_usuarioDesc (String titulo);
 */
 }
+
+// '' OR 1=1;
