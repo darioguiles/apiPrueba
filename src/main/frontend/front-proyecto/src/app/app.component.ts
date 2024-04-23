f9e9755221a0a9eb49caaa1298b8d17c431244e8
darioguiles <dario.guilescuesta@hotmail.com>
2024-04-23 14:37:23 +0200

    Se omite un poco el testeo en pos de iniciar con el front-end

--- src/main/frontend/front-proyecto/src/app/app.component.ts
+++ src/main/frontend/front-proyecto/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavegadorComponent} from "./navegador/navegador.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavegadorComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/main/frontend/front-proyecto/src/app/app.routes.ts
+++ src/main/frontend/front-proyecto/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {LoginComponent} from "./login/login.component";
+import {ContenidoComponent} from "./contenido/contenido.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'landing', component:LandingComponent},
+    {path:'login', component: LoginComponent},
+    {path:'content', component: ContenidoComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'landing'}
+];
